3c2ccd4088eb072adceec3d8d7119110dd939b2b
Kristo Palo <kristopalo@hotmail.com>
2017-09-02 20:25:30 +0300

    Default sorting implemented

--- src/main/java/com/palo/editor/view/EditorController.java
+++ src/main/java/com/palo/editor/view/EditorController.java
@@ -14,75 +14,74 @@ import javafx.scene.control.TextField;
 import javafx.util.Callback;
 
 public class EditorController {
-	
+
 	@FXML
 	private TextField filterField;
-	
+
 	@FXML
 	private TableView<Item> itemTable;
-	
+
 	private MainApp mainApp;
-	
-	public EditorController() {	
+
+	public EditorController() {
 	}
-	
+
 	@FXML
-	private void initialize() {	
-		// key column
+	private void initialize() {
+
 		TableColumn<Item, String> keyColumn = new TableColumn<Item, String>("Key");
 		keyColumn.setCellValueFactory(
 				new Callback<TableColumn.CellDataFeatures<Item, String>, ObservableValue<String>>() {
-					public ObservableValue<String> call(
-							TableColumn.CellDataFeatures<Item, String> e) {
+					public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> e) {
 						return new SimpleStringProperty(e.getValue().getKey());
 					}
 				});
 		itemTable.getColumns().add(keyColumn);
-		
-		// language file columns
+
 		for (final String filename : MainApp.arr) {
 			TableColumn<Item, String> languageColumn = new TableColumn<Item, String>(
 					filename.replace(".properties", ""));
 			languageColumn.setCellValueFactory(
 					new Callback<TableColumn.CellDataFeatures<Item, String>, ObservableValue<String>>() {
-						public ObservableValue<String> call(
-								TableColumn.CellDataFeatures<Item, String> p) {
+						public ObservableValue<String> call(TableColumn.CellDataFeatures<Item, String> p) {
 							return new SimpleStringProperty(p.getValue().fetchValue(filename));
 						}
 					});
 			itemTable.getColumns().add(languageColumn);
 		}
-		
-		
+
+		keyColumn.setSortType(TableColumn.SortType.ASCENDING);
+		itemTable.getSortOrder().add(keyColumn);
 	}
-	
+
 	public void setMainApp(MainApp mainApp) {
-        this.mainApp = mainApp;
+		this.mainApp = mainApp;
 
 		FilteredList<Item> filteredData = new FilteredList<>(this.mainApp.getItems(), item -> true);
-		
+
 		filterField.textProperty().addListener((observable, oldValue, newValue) -> {
-			
+
 			filteredData.setPredicate(item -> {
-				
+
 				if (newValue == null || newValue.isEmpty()) {
 					return true;
 				}
-				
+
 				String lowerCaseFilter = newValue.toLowerCase();
-				
+
 				if (item.getKey().toLowerCase().contains(lowerCaseFilter)) {
 					return true;
 				}
 				return false;
-				
+
 			});
-			
+
 		});
-		
+
 		SortedList<Item> sortedData = new SortedList<>(filteredData);
 		sortedData.comparatorProperty().bind(itemTable.comparatorProperty());
 		itemTable.setItems(sortedData);
-    }
-	
+
+	}
+
 }
